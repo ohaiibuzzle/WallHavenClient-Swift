3de00172a10d734d9e36eb1ae78dad130923b422
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-04-15 12:01:17 +0700

    Initial Commit

---
+++ WallHavenClient/WallHavenClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallHavenClientApp.swift
+//  WallHavenClient
+//
+//  Created by Venti on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WallHavenClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
